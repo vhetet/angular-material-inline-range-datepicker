897061910b3a85927e3861accde5d3f76abaa77d
Vince Hetet <vincent.hetet@usfoods.com>
2021-11-05 10:36:17 -0500

    add range to the date picker

--- src/app/datepicker-inline-calendar-example.ts
+++ src/app/datepicker-inline-calendar-example.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DateRange } from '@angular/material/datepicker';
 
 /** @title Datepicker inline calendar example */
 @Component({
@@ -8,6 +9,24 @@ import {Component} from '@angular/core';
 })
 export class DatepickerInlineCalendarExample {
   selected: Date | null;
+  @Input() selectedRangeValue: DateRange<Date> | undefined;
+  @Output() selectedRangeValueChange = new EventEmitter<DateRange<Date>>();
+
+  selectedChange(m: any) {
+    if (!this.selectedRangeValue?.start || this.selectedRangeValue?.end) {
+      this.selectedRangeValue = new DateRange<Date>(m, null);
+    } else {
+      const start = this.selectedRangeValue.start;
+      const end = m;
+      if (end < start) {
+        this.selectedRangeValue = new DateRange<Date>(end, start);
+      } else {
+        this.selectedRangeValue = new DateRange<Date>(start, end);
+      }
+    }
+    this.selectedRangeValueChange.emit(this.selectedRangeValue);
+    console.log({ range: this.selectedRangeValue });
+  }
 }
 
 
